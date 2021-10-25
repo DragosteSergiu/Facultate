d6263b430ff599ed13dea7db82756799c36c007a
DragosteSergiu <72548598+DragosteSergiu@users.noreply.github.com>
2021-10-25 19:55:11 +0300

    Update Lab2v2FLCD.py

--- Lab2v2FLCD.py
+++ Lab2v2FLCD.py
@@ -3,27 +3,33 @@ class Element:
         self.__position = position
         self.__token = token
 
-    def getPosition(self):
+    @property
+    def position(self):
         return self.__position
+    @position.setter
+    def position(self, value):
+        self.__position = value
 
-    def setPosition(self, position):
-        self.__position = position
-
-    def getToken(self):
+    @property
+    def token(self):
         return self.__token
-
-    def setToken(self, token):
-        self.__token = token
+    @token.setter
+    def token(self, value):
+        self.__token = value
 
 
 class SortedTable:
     def __init__(self):
         self.__table = []
-        
+
+    @property
+    def table(self):
+        return self.__table
+
     def isUniqueToken(self, token):
         exists = True
-        for element in self.__table: 
-            if element.getToken() == token:
+        for element in self.__table:
+            if element.token == token:
                 exists = False
         return exists
 
@@ -32,13 +38,15 @@ class SortedTable:
             element = Element(1, token)
             self.__table.append(element)
         else:
-            if (self.isUniqueToken(token) == True):
+            if self.isUniqueToken(token) == True:
                 self.insertElement(token)
 
     def determinePosition(self, token):
         position = 0
         while True:
-            if self.__table[position].getToken() > token:
+            if position == len(self.__table):
+                return position
+            if self.__table[position].token > token:
                 return position
             position = position + 1
 
@@ -46,24 +54,24 @@ class SortedTable:
         position = self.determinePosition(token)
         element = Element(position + 1, token)
         for elem in self.__table:
-            if elem.getToken() > token:
-                pos = elem.getPosition()
-                elem.setPosition(pos + 1)
+            if elem.token > token:
+                pos = elem.position
+                elem.position = (pos + 1)
         self.__table.insert(position, element)
 
     def searchByToken(self, token):
         exists = False
         for element in self.__table:
-            if element.getToken() == token:
+            if element.token == token:
                 exists = True
-                return element
+                return element.position
         if exists == False:
-            return None
+            return -1
 
-    def toString(self):
+    def __str__(self):
         toStr = ""
         for element in self.__table:
-            toStr = toStr + "Position: " + str(element.getPosition()) + ", Identifier; " + element.getToken() + "\n"
+            toStr = toStr + "Position: " + str(element.position) + ", Identifier: " + str(element.token) + "\n"
         return toStr
 
 
@@ -75,7 +83,7 @@ def main():
     st.addElement("aaa")
     print(st.toString())
     element = st.searchByToken("d")
-    print('SEARCH RESULT: ' + str(element.getPosition()) + ", " + element.getToken())
+    print(element)
 
 
 if __name__ == "__main__":
